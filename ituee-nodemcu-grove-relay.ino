e4ccb6e9ae6b88bedd553f966ed48cb6cbabd81b
Burak Topbastekin <buraktt@gmail.com>
2021-05-27 18:57:14 +0300

    updates & comments

--- ituee-nodemcu-grove-relay.ino
+++ ituee-nodemcu-grove-relay.ino
@@ -2,57 +2,74 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-
-#define WLAN_SSID       "SSID"
-#define WLAN_PASS       "PASS"
+// wi-fi credentials
+#define WLAN_SSID "SSID"
+#define WLAN_PASS "PASS"
 
 // Adafruit IO
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  8883
-#define AIO_USERNAME    "USERNAME"
-#define AIO_KEY         "KEY"
+#define AIO_SERVER "io.adafruit.com"
+#define AIO_SERVERPORT 8883
+#define AIO_USERNAME "USERNAME"
+#define AIO_KEY "KEY"
+
+// io.adafruit.com SHA1 fingerprint
+static const char *fingerprint PROGMEM = "59 3C 48 0A B1 8B 39 4E 0D 58 50 47 9A 13 55 60 CC A0 1D AF";
 
 //WiFiClient client;
 WiFiClientSecure client;
 
+// init adafruit client & topic subscription
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 Adafruit_MQTT_Subscribe infrared = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/infrared");
 
+// relay pin
 #define RELAY_PIN D3
 
 void MQTT_connect();
 
-void MQTT_connect() {
+void MQTT_connect()
+{
   int8_t ret;
-  if (mqtt.connected()) {
+  if (mqtt.connected())
+  {
     return;
   }
   Serial.print("Connecting to MQTT... ");
   uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) {
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);
-       retries--;
-       if (retries == 0) {
-         while (1);
-       }
+  while ((ret = mqtt.connect()) != 0)
+  {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);
+    retries--;
+    if (retries == 0)
+    {
+      while (1)
+        ;
+    }
   }
   Serial.println("MQTT Connected!");
 }
 
-void setup() {
+void setup()
+{
+  // Start serial connection
   Serial.begin(115200);
   delay(10);
 
+  // set relay pin as output
   pinMode(RELAY_PIN, OUTPUT);
 
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
 
+  // connect to wifi
   WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+
+  // loop until connected
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -62,32 +79,48 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
+  // check the fingerprint of io.adafruit.com's SSL cert
+  client.setFingerprint(fingerprint);
+
+  // subscribe to topic for new messages
   mqtt.subscribe(&infrared);
 }
 
 int x = 260;
 
-void loop() {
+void loop()
+{
 
+  // always keep connected
   MQTT_connect();
 
   Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(5000))) {
-    if (subscription == &infrared) {
+
+  // listen for new messages
+  while ((subscription = mqtt.readSubscription(5000)))
+  {
+    if (subscription == &infrared)
+    {
+      // get last reading
       x = atoi((char *)infrared.lastread);
       Serial.print(F("Got: "));
       Serial.println(x);
-      if (x < 260) {
+      // if ir level is lower than 260 (sun is setting)
+      // open relay
+      if (x < 260)
+      {
         digitalWrite(RELAY_PIN, HIGH);
-      } else {
+      }
+      else
+      {
         digitalWrite(RELAY_PIN, LOW);
       }
     }
   }
 
-  if(! mqtt.ping()) {
+  // if no ping from server, disconnect
+  if (!mqtt.ping())
+  {
     mqtt.disconnect();
   }
 }
-
-
--- ituee-nodemcu-grove-sunlight.ino
+++ ituee-nodemcu-grove-sunlight.ino
@@ -4,52 +4,60 @@
 #include "Adafruit_MQTT_Client.h"
 #include "SI114X.h"
 
-#define WLAN_SSID       "SSID"
-#define WLAN_PASS       "PASS"
+#define WLAN_SSID "SSID"
+#define WLAN_PASS "PASS"
 
 // Adafruit IO
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "USERNAME"
-#define AIO_KEY         "KEY"
+#define AIO_SERVER "io.adafruit.com"
+#define AIO_SERVERPORT 1883 // 8333 for secure connection
+#define AIO_USERNAME "USERNAME"
+#define AIO_KEY "KEY"
 
 WiFiClient client;
 //WiFiClientSecure client;
+
+// io.adafruit.com SHA1 fingerprint
+static const char *fingerprint PROGMEM = "59 3C 48 0A B1 8B 39 4E 0D 58 50 47 9A 13 55 60 CC A0 1D AF";
 
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 Adafruit_MQTT_Publish infrared = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/infrared");
 
 SI114X SI1145 = SI114X();
 
-
-
 void MQTT_connect();
 
-void MQTT_connect() {
+void MQTT_connect()
+{
   int8_t ret;
-  if (mqtt.connected()) {
+  if (mqtt.connected())
+  {
     return;
   }
   Serial.print("Connecting to MQTT... ");
   uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) {
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);
-       retries--;
-       if (retries == 0) {
-         while (1);
-       }
+  while ((ret = mqtt.connect()) != 0)
+  {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);
+    retries--;
+    if (retries == 0)
+    {
+      while (1)
+        ;
+    }
   }
   Serial.println("MQTT Connected!");
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   delay(10);
 
-  while (!SI1145.Begin()) {
+  while (!SI1145.Begin())
+  {
     Serial.println("Si1145 is not ready!");
     delay(1000);
   }
@@ -60,7 +68,8 @@ void setup() {
   Serial.println(WLAN_SSID);
 
   WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -69,11 +78,15 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  // check the fingerprint of io.adafruit.com's SSL cert
+  client.setFingerprint(fingerprint);
 }
 
-uint32_t x=0;
+uint32_t x = 0;
 
-void loop() {
+void loop()
+{
 
   MQTT_connect();
 
@@ -82,9 +95,12 @@ void loop() {
   Serial.print(F("\nSending infrared val "));
   Serial.print(x);
   Serial.print("...");
-  if (! infrared.publish(x)) {
+  if (!infrared.publish(x))
+  {
     Serial.println(F("Failed"));
-  } else {
+  }
+  else
+  {
     Serial.println(F("OK!"));
   }
   delay(2000);
@@ -94,5 +110,3 @@ void loop() {
   }
   */
 }
-
-
